951fd26390e1b51449411996db2c6d46c83b1956
Adrian Hangan <iovuadrian@gmail.com>
2018-06-21 16:33:42 +0300

    reformat code

--- etools-info-tooltip.js
+++ etools-info-tooltip.js
@@ -1,4 +1,4 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-tooltip/paper-tooltip.js';
 import '@polymer/iron-icons/iron-icons.js';
 import '@polymer/iron-flex-layout/iron-flex-layout.js';
@@ -13,76 +13,77 @@ import '@polymer/neon-animation/neon-animations.js';
  * @demo demo/index.html
  */
 class EtoolsInfoTooltip extends PolymerElement {
-  static get template() {
-    return html`
-    <style>
-      [hidden] {
-        display: none !important;
-      }
+    static get template() {
+        // language=HTML
+        return html`
+          <style>
+            [hidden] {
+              display: none !important;
+            }
 
-      :host {
-        --tooltip-box-style: {
-          text-align: center;
-          line-height: 1.4;
-        };
-        --light-tooltip-style: {
-          -webkit-box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
-          -moz-box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
-          box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
-          border: 1px solid rgba(0, 0, 0, 0.15);
-        };
-        @apply --layout-horizontal;
-        @apply --layout-center;
-      }
+            :host {
+              --tooltip-box-style: {
+                text-align: center;
+                line-height: 1.4;
+              };
+              --light-tooltip-style: {
+                -webkit-box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
+                -moz-box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
+                box-shadow: 0 6px 12px rgba(0, 0, 0, 0.175);
+                border: 1px solid rgba(0, 0, 0, 0.15);
+              };
+              @apply --layout-horizontal;
+              @apply --layout-center;
+            }
 
-      :host([right-aligned]) {
-        @apply --layout-end-justified;
-      }
+            :host([right-aligned]) {
+              @apply --layout-end-justified;
+            }
 
-      :host([icon-first]) {
-        @apply --layout-horizontal-reverse;
-        @apply --layout-end-justified;
-      }
+            :host([icon-first]) {
+              @apply --layout-horizontal-reverse;
+              @apply --layout-end-justified;
+            }
 
-      :host([icon-first][right-aligned]) {
-        @apply --layout-start-justified;
-      }
+            :host([icon-first][right-aligned]) {
+              @apply --layout-start-justified;
+            }
 
-      :host([theme="light"]) {
-        --paper-tooltip-background: var(--primary-background-color, #ffffff);
-        --paper-tooltip-opacity: 1;
-        --paper-tooltip-text-color: var(--primary-text-color, rgba(0, 0, 0, 0.87));
+            :host([theme="light"]) {
+              --paper-tooltip-background: var(--primary-background-color, #ffffff);
+              --paper-tooltip-opacity: 1;
+              --paper-tooltip-text-color: var(--primary-text-color, rgba(0, 0, 0, 0.87));
 
-        --paper-tooltip: {
-          @apply --tooltip-box-style;
-          @apply --light-tooltip-style;
-        };
-      }
+              --paper-tooltip: {
+                @apply --tooltip-box-style;
+                @apply --light-tooltip-style;
+              };
+            }
 
-      :host([form-field-align]) #tooltip-trigger {
-        @apply --layout;
-        @apply --layout-self-end;
-        margin-bottom: 11px;
-      }
+            :host([form-field-align]) #tooltip-trigger {
+              @apply --layout;
+              @apply --layout-self-end;
+              margin-bottom: 11px;
+            }
 
-      :host(:not([icon-first])) #tooltip-trigger {
-        margin-left: 8px;
-        @apply --etools-tooltip-trigger-icon;
-      }
+            :host(:not([icon-first])) #tooltip-trigger {
+              margin-left: 8px;
+              @apply --etools-tooltip-trigger-icon;
+            }
 
-      :host([icon-first]) #tooltip-trigger {
-        margin-left: 0;
-        margin-right: 8px;
-        @apply --etools-tooltip-trigger-icon;
-      }
+            :host([icon-first]) #tooltip-trigger {
+              margin-left: 0;
+              margin-right: 8px;
+              @apply --etools-tooltip-trigger-icon;
+            }
 
-      :host([important-warning]:not([hide-tooltip])) {
-        color: var(--error-color, #e54f2e);
-      }
-    </style>
-    <!-- element assigned to this tooltip -->
-    <slot name="field"></slot>
-    <span id="tooltip-trigger" hidden\$="[[hideTooltip]]">
+            :host([important-warning]:not([hide-tooltip])) {
+              color: var(--error-color, #e54f2e);
+            }
+          </style>
+          <!-- element assigned to this tooltip -->
+          <slot name="field"></slot>
+          <span id="tooltip-trigger" hidden\$="[[hideTooltip]]">
       <template is="dom-if" if="[[!customIcon]]" restamp="">
         <iron-icon icon="[[icon]]"></iron-icon>
       </template>
@@ -90,76 +91,78 @@ class EtoolsInfoTooltip extends PolymerElement {
         <slot name="custom-icon"></slot>
       </template>
     </span>
-    <paper-tooltip id="tooltip" for="tooltip-trigger" position="[[position]]" fit-to-visible-bounds="[[fitToVisibleBounds]]">
-      <slot name="message"></slot>
-    </paper-tooltip>
-`;
-  }
-
-  static get is() {
-    return 'etools-info-tooltip';
-  }
-
-  static get properties() {
-    return {
-      position: {
-        type: String,
-        value: 'top'
-      },
-      icon: {
-        type: String,
-        value: 'info-outline'
-      },
-      customIcon: {
-        type: Boolean,
-        value: false
-      },
-      hideTooltip: Boolean,
-      importantWarning: {
-        type: Boolean,
-        value: false,
-        reflectToAttribute: true,
-        observer: '_refreshStyles'
-      },
-      theme: {
-        type: String,
-        value: 'dark',
-        reflectToAttribute: true,
-        observer: '_refreshStyles'
-      },
-      fitToVisibleBounds: {
-        type: Boolean,
-        value: true
-      },
-      /**
-       * Used to align tooltip icon near a paper-input or a form input that uses paper-input-container
-       */
-      formFieldAlign: {
-        type: Boolean,
-        value: false,
-        reflectToAttribute: true
-      }
-    };
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    let tooltipContent = this.$.tooltip.$.tooltip;
-    if (tooltipContent) {
-      tooltipContent.style.display = 'flex';
-      tooltipContent.style.flexDirection = 'row';
-      tooltipContent.style.textAlign = 'left';
-      tooltipContent.style.lineHeight = '16px';
-      tooltipContent.style.whiteSpace = 'pre-wrap';
+          <paper-tooltip id="tooltip" for="tooltip-trigger" position="[[position]]"
+                         fit-to-visible-bounds="[[fitToVisibleBounds]]">
+            <slot name="message"></slot>
+          </paper-tooltip>
+        `;
+        // language=HTML
     }
-  }
 
-  _refreshStyles(importantWarning) {
-    if (importantWarning === undefined) {
-      return;
+    static get is() {
+        return 'etools-info-tooltip';
     }
-    this.updateStyles();
-  }
+
+    static get properties() {
+        return {
+            position: {
+                type: String,
+                value: 'top'
+            },
+            icon: {
+                type: String,
+                value: 'info-outline'
+            },
+            customIcon: {
+                type: Boolean,
+                value: false
+            },
+            hideTooltip: Boolean,
+            importantWarning: {
+                type: Boolean,
+                value: false,
+                reflectToAttribute: true,
+                observer: '_refreshStyles'
+            },
+            theme: {
+                type: String,
+                value: 'dark',
+                reflectToAttribute: true,
+                observer: '_refreshStyles'
+            },
+            fitToVisibleBounds: {
+                type: Boolean,
+                value: true
+            },
+            /**
+             * Used to align tooltip icon near a paper-input or a form input that uses paper-input-container
+             */
+            formFieldAlign: {
+                type: Boolean,
+                value: false,
+                reflectToAttribute: true
+            }
+        };
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        let tooltipContent = this.$.tooltip.$.tooltip;
+        if (tooltipContent) {
+            tooltipContent.style.display = 'flex';
+            tooltipContent.style.flexDirection = 'row';
+            tooltipContent.style.textAlign = 'left';
+            tooltipContent.style.lineHeight = '16px';
+            tooltipContent.style.whiteSpace = 'pre-wrap';
+        }
+    }
+
+    _refreshStyles(importantWarning) {
+        if (importantWarning === undefined) {
+            return;
+        }
+        this.updateStyles();
+    }
 }
 
 window.customElements.define(EtoolsInfoTooltip.is, EtoolsInfoTooltip);
