d7e57839376c20947bf6f3a7b84ac3b1bd5dc0f3
Mercea Emil <office@insightfeatures.com>
2022-11-08 11:26:19 +0200

    [ch31395] Missing Russian translations part IV (Government)

---
+++ assets/translations.js
@@ -0,0 +1,25 @@
+const translations = {
+  en: {
+    CLOSE: 'Close'
+  },
+  ro: {
+    CLOSE: 'Închide'
+  },
+  fr: {
+    CLOSE: 'Fermer'
+  },
+  ar: {
+    CLOSE: 'قريب'
+  },
+  es: {
+    CLOSE: 'Cerca'
+  },
+  pt: {
+    CLOSE: 'Perto'
+  },
+  ru: {
+    CLOSE: 'закрывать'
+  }
+};
+
+export default translations;
